677c32c9cd2ac6d348719dd1800393615b3c507c
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2023-07-25 05:55:09 +0500

    add archlinux_container script

---
+++ archlinux_container.sh
@@ -0,0 +1,16 @@
+# Moving to a /tmp DIR
+#
+echo "Moving inside /tmp DIR"
+cd /tmp
+
+# Cloning the yay-bin repo
+echo "Cloning the yay-bin"
+git clone https://aur.archlinux.org/yay-bin.git
+
+# Installing yay-bin
+#
+echo "Moving inside yay-bin DIR"
+cd /tmp/yay-bin/
+
+echo "Building and Installing yay-bin"
+makepkg -sri
