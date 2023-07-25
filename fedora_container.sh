4dd572c74fa55bb623e51deba78be6555ced5051
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2023-07-25 05:39:11 +0500

    Create fedora_container.sh

---
+++ fedora_container.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo dnf upgrade
+
+# zsh, zsh-autosuggestions, zsh-syntax-highlighting, zoxide
+#
+echo "Installing zsh, zsh-autosuggestions, zsh-syntax-highlighting Zoxide"
+sudo dnf install zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
+
+# VS Code
+#
+echo "Enabling Visual Studio Code Repo Fedora Container"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+echo "Intalling the Visual Studio Code binary on Fedora Container"
+sudo dnf check-update
+sudo dnf instal code
+
+# Starship
+#
+echo "Adding Starship repo and Installing the package"
+sudo dnf copr enable atim/starship
+sudo dnf install starship
