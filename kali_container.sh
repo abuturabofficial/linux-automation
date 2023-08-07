c97e9e0453c6422dded966293d68c7512f1c56f4
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2023-08-07 09:53:08 +0500

    add script for kali linux container

---
+++ kali_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Installing some Basic Utilities
+#
+sudo apt install neovim tealdeer zoxide zsh-autosuggestions zsh-syntax-highlighting bat
+
+# Some Basic GPU Tools to access Hardware
+#
+sudo apt install kali-tools-gpu
+
+# Command Not found handler
+#
+sudo apt install command-not-found
