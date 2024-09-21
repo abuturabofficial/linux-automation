341129b054888b5820244a46af3c32bee8245050
AbuTurab <68259771+abuturabofficial@users.noreply.github.com>
2024-09-21 08:52:12 +0500

    Update Kali script

--- kali_container.sh
+++ kali_container.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
-# Installing some Basic Utilities
-#
-sudo apt install neovim tealdeer zoxide zsh-autosuggestions zsh-syntax-highlighting bat
-
-# Command Not found handler
-#
-sudo apt install command-not-found
+# Check if we are in Kali Linux
+if [ -f /etc/os-release ] && grep -q "Kali" /etc/os-release; then
+    # Check if the packages are not already installed
+    if ! dpkg -s neovim tealdeer zoxide zsh-autosuggestions zsh-syntax-highlighting bat >/dev/null 2>&1; then
+        # Install zsh-related packages if shell is 'zsh'
+        if [ "$SHELL" = "/usr/bin/zsh" ]; then
+            sudo apt install zsh-autosuggestions zsh-syntax-highlighting
+        fi
+        sudo apt install neovim tealdeer zoxide bat
+    else
+        echo "Packages are already installed. Skipping installation."
+    fi
+else
+    echo "Not running on Kali Linux. Skipping installation."
+fi
